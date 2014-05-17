19941b9dbc8ba3a9ee30c4c8adb1a0190e09f95f
mariangelds <mariangel.ds@gmail.com>
2014-05-17 11:21:40 -0430

    Inicio del Proyecto Pitagoras

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Proyecto1::Application.config.secret_key_base = 'b027cc1efaa2b01f113f2a420ff8419af919ff496a1e0551640f41aec02e8247365a3d0d9018da75017701db3baf2b0203c72849723635a2ad4115948bea4e87'
